4d9c9d22980a283d6e7fe1bdd2dae815fa46a3df
PRIYANKjakharia <priyankjakharia2005@gmail.com>
2026-06-15 13:30:05 +0530

    Day 5 Phase 2

--- DTOs/LearningTask DTOs/LearningTaskResponse.cs
+++ DTOs/LearningTask DTOs/LearningTaskResponse.cs
@@ -6,11 +6,11 @@ namespace TraineeManagement.API.DTOs;
 public class LearningTaskResponse
 {
     public int Id { get; set; }
-    public string Title { get; set; }
+    public string? Title { get; set; }
 
-    public string Description { get; set; }
+    public string? Description { get; set; }
 
-    public string ExpectedTechStack { get; set; }
+    public string? ExpectedTechStack { get; set; }
 
     public DateTime DueDate { get; set; }
     public string? Status { get; set; }
